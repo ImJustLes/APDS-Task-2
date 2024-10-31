f52431e4edb7636ef64720018d3981b5a8cc1e95
ImJustLes <ST10079848@vcconnect.edu.za>
2024-10-31 18:15:35 +0200

    Added error handling to ensure user's fill in all required fields and a logged in user can't make a payement to themselves

--- Index.js
+++ Index.js
@@ -12,6 +12,12 @@ var bruteForce = new expressBrute(store)
 
 app.use(express.json())
 
+let user = {
+    name: '',
+    accountnumber: '',
+    password: ''
+}  
+
 app.post("/login", bruteForce.prevent, async (req, res) => {
 
  // Updated regex pattern: at least 4 characters, at least one special character
@@ -19,12 +25,13 @@ app.post("/login", bruteForce.prevent, async (req, res) => {
  const collection = await db.collection('BankCustomers');
 
  try {
-     const user = {
+     user = {
          name: req.body.name,
          accountnumber: req.body.accountnumber,
          password: req.body.password
      }        
 
+     if (user.name && user.accountnumber && user.password) {
      // Check if the password meets the regex requirements
      if (passwordRegex.test(user.password)) {
          // Attempt to find the user in the database
@@ -50,6 +57,9 @@ app.post("/login", bruteForce.prevent, async (req, res) => {
          // Password doesn't meet regex requirements
          res.status(400).json({ message: 'Invalid password format' })
      }
+    } else {
+        res.status(400).json({ message: 'Please input all required fields.' })
+    }
  } catch (e) {
      console.error('Error occurred during login: ', e);
      res.status(500).json({ message: 'Internal server error' })
@@ -67,7 +77,6 @@ app.post("/elogin", bruteForce.prevent, async (req, res) => {
     try {
         const user = {
             name: req.body.name,
-            accountnumber: req.body.accountnumber,
             password: req.body.password
         }        
    
@@ -158,7 +167,55 @@ app.post('/signup', async (req, res) => {
 
     const existingUser = await collection.findOne({ email: userModel.email })
 
-    if(!existingUser) {
+    if(userModel.id && userModel.accountnumber && userModel.name && userModel.surname && userModel.email && userModel.password){
+    
+        if(!existingUser) {
+
+            if(passwordRegex.test(userModel.password) && emailRegex.test(userModel.email)) {
+                    
+                const hashedPassword = await bcrypt.hash(req.body.password, 10)  // Hash the password with salt
+                userModel.password = hashedPassword
+
+                const result = await collection.insertOne(userModel)   
+                res.status(201).send(result)
+                console.log(`Password for user ${req.body.email} hashed successfully`)
+                
+            } else {
+                res.status(401).json({ message: 'Please input an appropriate email and/or password' })
+            }  
+        } else {
+            res.status(404).json({ message: 'This user already exists' })
+        } 
+    } else {
+        
+    }    res.status(404).json({ message: 'Please fill in all the required fields' })
+
+    } catch (err) {
+        console.error(err)
+        res.status(500).send({ message: 'Error during signup' })
+    }
+})
+
+app.post('/esignup', async (req, res) => {
+
+    const passwordRegex = /^(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{4,}$/;
+    const emailRegex = /^.*?@(?:gmail\.com|live\.co\.za|vcconnect\.edu\.za|varsitycollege\.co\.za|outlook\.com)$/;
+    const collection = await db.collection('BankEmployees')   
+
+    try {
+             
+        // Creating the user model with the hashed password
+        const userModel = {
+            id: req.body.id,
+            name: req.body.name,
+            surname: req.body.surname,
+            email: req.body.email,
+            password: req.body.password
+        }
+
+    const existingUser = await collection.findOne({ email: userModel.email })
+
+    if(existingUser) {
 
         if(passwordRegex.test(userModel.password) && emailRegex.test(userModel.email)) {
                 
@@ -195,15 +252,39 @@ app.post('/payement', /* checkAuth */ async (req, res) => {
             code: req.body.code
         }
 
-        const collection = db.collection('PendingPayements')
-        const result = await collection.insertOne(payement)
-        res.status(201).send(result)
+        const collection = await db.collection('BankCustomers') 
+        const existingUser = await collection.findOne({ accountnumber: payement.accountnumber })      
+        
+        if(payement.amount && payement.currency && payement.provider && payement.name && payement.accountnumber && payement.code) 
+        {
+            if (existingUser) 
+            {
+                if (user.accountnumber != payement.accountnumber && existingUser.name == payement.name){
 
+                    const collection = db.collection('PendingPayements')
+                    const result = await collection.insertOne(payement)
+                    console.log('Payement successful!')
+                    res.status(201).send(result)
+                }    
+                else
+                {
+                    res.status(404).json({ message: 'Account number and/or name is invalid.' })
+                }            
+            }
+            else 
+            {
+                res.status(404).json({ message: 'User you are trying to send a payement to does not exist.' })
+            }
+        }  
+        else
+        {
+            res.status(500).send({ message: 'Please fill in all fields.' })
+        } 
     } 
     catch (e) 
     {
         console.error('Error occurred while pushing: ', e)
-        res.status(500).send('Internal server error')
+        res.status(500).send('Internal server error.')
     }
 })
 
